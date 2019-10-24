15474e198a6ce4550f1ac7b3a3d49ca8fe27d0d6
MahmoudMabrok <mahmoudmabrok3579@gmail.com>
2019-10-24 09:54:35 +0200

    add reactstrap & menu 1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
 
 import './App.css'
 import { Menu } from './components/MenuComponent';
@@ -8,7 +9,12 @@ class App extends Component {
   render() {
     return (
       <div>
-        <NavBar />
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/" > Restauran Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        
         <Menu />
       </div>
     );
